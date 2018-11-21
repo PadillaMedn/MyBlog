c66f91a4a630406911754610f475897f3d3e0f99
padilla mend <erickpadillamendoza@gmail.com>
2018-11-20 21:39:58 -0600

    utilizando strong params

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :body, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :body, presence: true, length: {minimum:20}
 end
