37c4a11c6d0fe6bc4ebfdec6aa8d3515b56becb6
padilla mend <erickpadillamendoza@gmail.com>
2018-11-21 22:19:02 -0600

    utilizando scaffold

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,21 @@
 class ArticlesController < ApplicationController
+    before_action :authenticate_user!, except: [:show,:index]
+    before_action :set_article, except: [:index,:new,:create]
+   
     def index
      @articles = Article.all
     end
     def show
-     @article = Article.find(params[:id])
+        @article.update_visits_count
     end
     def new
       @article = Article.new
     end
     def edit
-      @article = Article.find(params[:id])
+   
     end
     def create
-        @article = Article.new(article_params)
+        @article = current_user.articles.new(article_params)
 
         if  @article.save
              redirect_to @article
@@ -21,12 +24,12 @@ class ArticlesController < ApplicationController
         end 
     end
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         redirect_to articles_path
     end
     def update
-        @article = Article.find(params[:id])
+       
         if @article.update(article_params)
             redirect_to @article
         else
@@ -35,6 +38,11 @@ class ArticlesController < ApplicationController
     end
 
     private
+
+    def set_article
+        @article = Article.find(params[:id])
+   end
+   
 
     def article_params
         params.require(:article).permit(:title,:body)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,21 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, uniqueness: true
     validates :body, presence: true, length: {minimum:20}
+
+    before_save :set_visit_count
+
+
+    def update_visits_count
+        self.visit_count ||= 0
+        self.update(visit_count: self.visit_count + 1)
+    end
+    
+
+    private
+    
+    def set_visit_count
+        self.visit_count ||= 0
+    end
+
 end
