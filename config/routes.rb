aa8ffbde03b06922dad27df016d04ffad16f1040
CDS023 <HP@CDS-AH-2011-023.sv.cds>
2018-11-26 16:35:56 -0600

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   devise_for :user
-  root 'articles#index'
+  root 'welcome#index'
   get 'myarticles', to: 'articles#myarticles'
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
