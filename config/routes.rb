57ff6da020501d82dfb5850a58b424887820858c
CDS023 <HP@CDS-AH-2011-023.sv.cds>
2018-11-26 16:28:27 -0600

    vistas

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   devise_for :user
-  root 'welcome#index'
+  root 'articles#index'
   get 'myarticles', to: 'articles#myarticles'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
